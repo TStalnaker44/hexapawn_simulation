92102466839576f8588619b9cb76b2217c1584aa
Trevor <stalnakert20@mail.wlu.edu>
2021-01-29 19:25:15 -0500

    Added learning to the hexapawn bots

--- hexapawn.py
+++ hexapawn.py
@@ -56,20 +56,29 @@ class Bot():
 
     def __init__(self):
         self._brain = createStateMapping()
-        self._memory = []
+        self._lastMove = None
+        self._forfeit = False
 
     def pickMove(self, turn, state):
         possibleMoves = self._brain[turn][state]
+        if possibleMoves == []:
+            self._forfeit = True
+            self._lastMove = None
+            return None #The bot forfeits in losing state
         move = random.choice(possibleMoves)
-        self._memory.append((turn, state, move))
+        self._lastMove = (turn, state, move)
         return move
 
     def learn(self, won):
-        ## Allow a toggle for this
-        ## Increase number of good moves
-        ## Decrease the number of bad moves
-        pass
-
+        ## TO-DO: Allow a toggle for this
+        if self._lastMove != None:
+            turn, state, move = self._lastMove
+            if won:
+                for x in range(3):
+                    self._brain[turn][state].append(move)  
+            else:
+                self._brain[turn][state].remove(move)
+        
 class Board():
 
     def __init__(self):
@@ -95,11 +104,24 @@ class Board():
         else:
             return False
 
+    def getWinner(self, turn):
+        if "1" in self._board[0:3]:
+            return 1
+        if "2" in self._board[6:9]:
+            return 2
+        if not "2" in self._board:
+            return 1
+        if not "1" in self._board:
+            return 2
+        if getMoves(self.getBoardState(), turn) == []:
+            return ((turn) % 2)+1
+
 def getStateVisualization(state):
     state = state.replace("2", "X")
     state = state.replace("1", "O")
     state = state.replace("0", " ")
-    return f"{state[:3]}\n{state[3:6]}\n{state[6:]}"
+    #return f"{state[:3]}\n{state[3:6]}\n{state[6:]}"
+    return ("%s\n%s\n%s" % (state[:3], state[3:6], state[6:]))
 
 def mirrorState(state):
     firstrow = state[0:3][::-1]
@@ -178,23 +200,59 @@ def createStateMapping():
                 states2moves[turn][mirroredState] = getMoves(mirroredState, turn)
     return states2moves
 
+def simulate(silent=False):
 
+    games = 100
+    
+    HIM = Bot()
+    HER = Bot()
 
-board = Board()
-HIM = Bot()
-HER = Bot()
-turn = 1
+    wins = {1:0, 2:0}
 
-while not board.isGameOver(turn):
-    print("Turn:", turn)
-    board.printBoard()
-    if turn % 2 == 1:
-        move = HIM.pickMove(turn, board.getBoardState())
-    else:
-        move = HER.pickMove(turn, board.getBoardState())
-    board.executeMove(move)
-    turn += 1
+    for x in range(games):
+        
+        board = Board()
+        turn = 1
+
+        if not silent:
+            print("Turn: 0")
+            board.printBoard()
+
+        while not board.isGameOver(turn):
+            if not silent:
+                print("Turn:", turn)
+            if turn % 2 == 1:
+                move = HIM.pickMove(turn, board.getBoardState())
+            else:
+                move = HER.pickMove(turn, board.getBoardState())
+            if move == None:
+                break
+            board.executeMove(move)
+            if not silent:
+                board.printBoard()
+            turn += 1
+
+        winner = board.getWinner(turn)
+        if HIM._forfeit:
+            winner = 2
+        if HER._forfeit:
+            winner = 1
+        HIM.learn(winner==1)
+        HER.learn(winner==2)
+        if not silent:
+            print("Player %d won the game" % (winner))
+
+        HIM._forfeit = False
+        HER._forfeit = False
+
+        wins[winner] += 1
+
+    print("HIM wins:", wins[1])
+    print("HER wins:", wins[2])
+    print("Of", games, "games")
 
   
+if __name__=="__main__":
+    simulate(True)
 
     
